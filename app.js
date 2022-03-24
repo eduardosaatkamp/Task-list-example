58404c7539e0b4c9f3173ebc5b82a571617486ef
Zack <zackschetine@gmail.com>
2022-03-24 16:25:45 -0300

    troca do botão pelo ícone, adição do traçado nas tarefas feitas

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 const switcher=document.querySelector('.btn');
 
 switcher.addEventListener('click',function(){
-    document.body.classList.toggle('dark-theme')
+    document.body.classList.toggle('dark-theme');
 
 /* precisa retornar depois para dark */
-var ClassName = document.body.className;
-if(ClassName== 'light-theme') 
-{ this.textContent = "Dark";}
-else{this.textContent = "Light";}
+var ClassName = document.body.className; 
+var icone = document.getElementById("icone");
+
+if(ClassName == 'light-theme')
+{icone.style.color = '#0400fb';} 
+else{icone.style.color = '#fae900';}
  console.log('current class name:'+ClassName);
 });
